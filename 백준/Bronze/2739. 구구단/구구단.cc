961be3587efbeba83173842e23d9a607bb08c504
Yang Donggyun <89330538+ydg0724@users.noreply.github.com>
2023-03-18 20:01:47 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a;
+    scanf("%d", &a);
+
+    for (int i = 1; i < 10; i++)
+    {
+        printf("%d * %d = %d\n", a, i,a * i);
+    }
+
+    return 0;
+}
