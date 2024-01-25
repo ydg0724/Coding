ec718601fb61e2b431f6fd558721f980d0028613
Yang Donggyun <89330538+ydg0724@users.noreply.github.com>
2024-01-26 03:30:00 +0900

    [level 1] Title: 추억 점수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,15 @@
+def solution(name, yearning, photo):
+    matching = {}   #딕셔너리 생성
+    answer = []
+    for i in range(len(name)):
+        matching[name[i]] = yearning[i] #딕셔너리 키 값 매핑
+    for i in range(len(photo)):
+        sums = 0
+        for j in photo[i]:
+            if j in matching.keys():
+                sums += matching[j]
+        answer.append(sums)
+
+        
+    
+    return answer
